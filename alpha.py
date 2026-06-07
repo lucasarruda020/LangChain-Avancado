5f117bf7257431f54cc0bd8fc5250d3548e9a6ce
Lucas Arruda <lucasarruda020@gmail.com>
2026-06-07 20:41:27 -0300

    embedding e chunks utilizando diversas formas, tanto por token, por paragrafo, por letras e tambem por separacao logica pela OpenAI ou HF

--- alpha.py
+++ alpha.py
@@ -27,25 +27,75 @@ from langchain_core.output_parsers import StrOutputParser
 
 # Carrega conteúdo de páginas web para processamento
 from langchain_community.document_loaders import WebBaseLoader
+
 # Carrega conteudo de um diretorio
 from langchain_community.document_loaders import DirectoryLoader
 
+# Juntar todos os codigos 
+from langchain_community.document_loaders.merge import MergedDataLoader
+
+# Rodar arquivos tipo texto
+from langchain_community.document_loaders import TextLoader
+
+# Baixa em paginas web 
+from langchain_community.document_loaders import RecursiveUrlLoader
+
+# Biblioteca para chunk de tokens
+from langchain_text_splitters import CharacterTextSplitter
+
+#hugging face
+from transformers import AutoTokenizer, AutoModel
+from huggingface_hub import login
 load_dotenv()
+
+login(token=os.getenv("HF_TOKEN"))
+
 os.environ.get("CHAVE_API_OPENAI")
-os.getenv("LANGSMITH_API_KEY")
-os.getenv("LANGCHAIN_TRACING_V2")
 api_key = os.getenv("CHAVE_API_OPENAI")
+
 
 modelo = ChatOpenAI(
     model='gpt-5-nano',
     temperature=0.2,
     api_key=api_key
 )
-
+# Ler apenas um doc
 documento = PyPDFLoader("documentos/GTB_gold_Nov23.pdf").load()
 
-url = "https://g1.globo.com/tecnologia/noticia/2026/06/05/dona-do-claude-sugere-pausa-no-desenvolvimento-da-ia-por-risco-de-sistemas-sairem-do-controle-humano.ghtml"
-
-news = WebBaseLoader(web_path=url).load()
-
+# baixar url
+#url = "https://g1.globo.com/tecnologia/noticia/2026/06/05/dona-do-claude-sugere-pausa-no-desenvolvimento-da-ia-por-risco-de-sistemas-sairem-do-controle-humano.ghtml"
+#news = WebBaseLoader(web_path=url).load()
+# Pegar tudo de um diretorio 
 pdfs = DirectoryLoader("/Users/lucasarruda/Study/Alura/LangChain_Tecnicas_avancadas_de_RAG/documentos", glob="*.pdf").load()
+
+# Juntar todos docs em um so
+#all_docs = MergedDataLoader(loaders=[WebBaseLoader(web_path=url), TextLoader("/Users/lucasarruda/Study/Alura/LangChain_Tecnicas_avancadas_de_RAG/documentos/GTB_gold_Nov23.txt", encoding='utf-8')])
+
+#all_loaders = all_docs.load()
+
+#print(len(all_loaders))
+
+## Baixar a pagina web
+#loader = RecursiveUrlLoader("https://platform.openai.com/usage")
+
+#lc_docs = loader.load()
+
+## Chunk por letra palavra
+#splitter = RecursiveCharacterTextSplitter(
+#   chunk_size=1000, chunk_overlap=100
+#)
+
+#pedacos = splitter.split_documents(pdfs)
+
+
+
+
+token_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    encoding_name="cl100k_base", chunk_size=8000, chunk_overlap=100
+)
+
+token_pedacos = token_splitter.split_documents(pdfs)
+
+emb_tokenzier = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-small')
+emb_model = AutoModel.from_pretrained('intfloat/multilingual-e5-small')
+
