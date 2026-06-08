94d2cb6ff7d07cd0db9f13bc5222b3c4c516b483
Lucas Arruda <lucasarruda020@gmail.com>
2026-06-07 21:00:50 -0300

    testando vector stores

--- alpha.py
+++ alpha.py
@@ -45,11 +45,21 @@ from langchain_text_splitters import CharacterTextSplitter
 
 #hugging face
 from transformers import AutoTokenizer, AutoModel
-from huggingface_hub import login
+
+# Fazer forma semantica langchain com chunks
+from langchain_experimental.text_splitter import SemanticChunker
+
+# forma semantica usando HF
+from langchain_huggingface import HuggingFaceEmbeddings
+
+# Banco vetorial de embeddings FAISS (Comum igual chroma)
+from langchain_community.vectorstores import FAISS
+
+# Banco vetorial tambem 
+from langchain_pinecone import PineconeVectorStore
+
+
 load_dotenv()
-
-login(token=os.getenv("HF_TOKEN"))
-
 os.environ.get("CHAVE_API_OPENAI")
 api_key = os.getenv("CHAVE_API_OPENAI")
 
@@ -96,6 +106,63 @@ token_splitter = CharacterTextSplitter.from_tiktoken_encoder(
 
 token_pedacos = token_splitter.split_documents(pdfs)
 
+# Baixar arquivos HF para token
 emb_tokenzier = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-small')
 emb_model = AutoModel.from_pretrained('intfloat/multilingual-e5-small')
 
+hf_splitter = CharacterTextSplitter.from_huggingface_tokenizer(
+    tokenizer=emb_tokenzier,
+    chunk_size=512, chunk_overlap=50
+)
+
+hf_pedacos = hf_splitter.split_documents(pdfs)
+len(hf_pedacos)
+
+
+# pega por blocos do chunks por um setor logico pela OpenAI
+semantic_openai_splitter = SemanticChunker(OpenAIEmbeddings())
+
+
+semantic_openai_pedacos = semantic_openai_splitter.split_documents(pdfs)
+
+
+print(len(semantic_openai_pedacos[2]))
+
+#mUsarnsetor logico para os chunks porem com HF
+hf_emb_model = HuggingFaceEmbeddings(model_name="infloat/multilingual-e5-small")
+semantic_hf_splitter = SemanticChunker(emb_model)
+semantic_hf_pedacos = semantic_hf_splitter.split_documents(pdfs)
+
+len(semantic_hf_pedacos)
+
+
+
+#from InMemoryVectorStore
+
+vectorStore = InMemoryVectorStore.from_documents(
+    documens=semantic_hf_pedacos, embedding=hf_emb_model
+)
+
+retriever = vectorStore.as_retriever(search_kwargs={"k":3})
+print(retriever.invoke("Seguro viagem"))
+
+
+# Utilizando FAISS & Demais ectore store
+
+
+vectorstore = FAISS.from_documents(
+    documents=semantic_hf_pedacos, embedding=hf_emb_model
+)
+
+retriever = vectorstore.as_retriever()
+
+print(retriever.invoke("Sala VIP"))
+
+pc_vector_store = PineconeVectorStore(
+    host="https/www.example.com",
+    pineconne_api_key="spk-3132190120312092",
+    embedding=hf_emb_model
+)
+
+pc_vector_store.add_documents(semantic_hf_pedacos)
+pc_vector_store.similarity_search_with_score("Seguro viagem") # sem usa retriever fazendo uma busca similar direto e dando resultado da distancia vetorial.
