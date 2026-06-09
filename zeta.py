cd8bb91baaeaffbd178b342007eb271d8e68ff31
Lucas Arruda <lucasarruda020@gmail.com>
2026-06-08 23:00:24 -0300

    novas tecnicas de validacao

--- zeta.py
+++ zeta.py
@@ -1,3 +1,5 @@
+import os
+import dotenv
 # baixa todos doc do diretorio
 from langchain_community.document_loaders import DirectoryLoader
 
@@ -22,6 +24,17 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 # Prompt de template comum 
 from langchain_core.prompts import PromptTemplate
+# chamand openAI
+from langchain_openai import ChatOpenAI
+# nova biblioteca para fazer multiQuerys
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+# preparar o datase para treinamento
+
+from langchain_community.evaluation.qa_evaluator import QAEvalChain
+from langchain.evaluation.qa import QAGenerateChain
+
+import json
 
 modelo = OllamaLLM(model="gemma3:4b")
 
@@ -99,17 +112,87 @@ rewritter_rag_chain = (
 
 rewritter_rag_chain.invoke(pergunta)
 
-## Generating Multiple queries
+## Generating Multiple queries para contextualizar a resposta
 
 multi_query_prompt_template = """
 Voce e um assistente de modelo de linguagem de IA. Sua tarefa e gerar versoes diferentes da pergunta do usuario para recuperar documentos relevantes de um banco de dados vetorial
 ao gerar multiplas perspectivas sobre a pergunta do usuario
 Pergunta original: {question}
-
 """
 
 multi_query_prompt = PromptTemplate.from_template(multi_query_prompt_template)
 
+multi_query_chain = multi_query_prompt | modelo | CommaSeparatedListOutputParser()
+
+multi_query_chain.invoke(pergunta)
 
 
-multi_query_chain = multi_query_prompt | llm || CommaSeparatedListOutputParser()
+
+multi_retriever = MultiQueryRetriever(
+    retriever=retriever, llm_chain=multi_query_chain
+)
+
+multi_rag_chain = (
+    {
+        "contexto": RunnablePassthrough() | multi_retriever,
+        "query": RunnablePassthrough()
+    } | prompt | modelo | StrOutputParser()
+)
+
+multi_rag_chain.invoke(pergunta)
+
+
+## Tecnica hyde
+
+hyde_prompt_template = """
+escreva um paragrafo que possa responder a pergunta apresentada, nao adicione informacoes
+Pergunta: {question}
+Paragrafo:
+"""
+
+hyde_prompt = PromptTemplate.from_template(hyde_prompt_template)
+
+hyde_chain = modelo | StrOutputParser(
+
+)
+
+hyde_rag_chain = (
+        {
+        "contexto": RunnablePassthrough() | hyde_chain | retriever,
+        "query": RunnablePassthrough()
+    }
+)
+
+multi_query_chain.invoke(pergunta)
+
+
+## Aula de validacao cap 4 Criando um file que de acordo com o chunk ele gera uma pergunta + uma resposta
+eval_chain = QAEvalChain.from_llm(modelo)
+
+def avaliar(perguntas_respostas, geracoes):
+    avaliacoes = eval_chain(perguntas_respostas, geracoes)
+    corretas = 0
+    for i in enumerate(perguntas_respostas):
+        corretas = corretas + (1 if avaliacoes[i]["results"].split("\n")[-1].split(":")[-1].strip() == "CORRECT" else 0)
+    return corretas/len(perguntas_respostas)
+
+
+qa_chain = QAGenerateChain.from_llm(modelo)
+
+perguntas_resposta = qa_chain.apply_and_parse(
+    [{"doc": p.page_content} for p in pedacos]
+)
+
+with open("qa_pais.json") as file:
+    pairs = json.load(file)
+
+len(pairs)
+
+geracoes_sem_rag = []
+for pr in perguntas_resposta[:10]:
+    geracoes_sem_rag.append({"result": modelo.invoke(pr["query"])})
+
+
+geracoes_sem_rag2 = [{"result": g["result"].content} for g in geracoes_sem_rag]
+
+avaliar(perguntas_resposta[:10], geracoes_sem_rag2)
